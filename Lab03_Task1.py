eabbb168b4e7971445d7e39623a3a94fe1d01856
alex4358 <104269530+alex4358@users.noreply.github.com>
2023-02-25 18:05:47 +0800

    Add files via upload

---
+++ Lab03_Task1.py
@@ -0,0 +1,26 @@
+intMonth=["2","1","3","4","5","6","7","8","9","10","11","12"]
+for j in intMonth:
+    if j == "1":
+        print("january")
+    elif j == "2":
+        print("February")
+    elif j =="3":
+        print("March")
+    elif j == "4":
+        print("April")
+    elif j == "5":
+        print("May")
+    elif j == "6":
+        print("June")
+    elif j == "7":
+        print("July")
+    elif j == "8":
+        print("August")
+    elif j == "9":
+        print("September")
+    elif j == "10":
+        print("October")
+    elif j == "11":
+        print("November")
+    elif j == "12":
+        print("December")
